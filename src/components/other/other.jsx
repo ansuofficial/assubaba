859e51b541a9ffc444db45add1e109b25b8a349d
Ansuofficial <badjieansu165@gmail.com>
2023-11-06 22:12:20 +0000

    assubaba

--- src/components/other/other.jsx
+++ src/components/other/other.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const otherPhonesData = [
   {
     img: "http://www.imagineonline.store/cdn/shop/files/iPhone_15_Pro_Max_Blue_Titanium_PDP_Image_Position-1__en-IN_493x.jpg",
@@ -33,24 +35,48 @@ const otherPhonesData = [
   },
 ];
 
+
 const OtherPhones = () => {
+  const [visible, setVisible] = useState(4);
+  const [showButton, setShowButton] = useState(true);
+
+  const showMore = () => {
+    setVisible((prevValue) => prevValue + 4);
+    setShowButton(false); // Hide the button after clicking
+  };
+
+  const showLess = () => {
+    setVisible((prevValue) => prevValue - 4);
+    setShowButton(true); // Show the "Show more..." button
+  };
+
   return (
     <div className="popular-phones-wrapper">
       <div className="other-phones-title">
         <h1>Other phones</h1>
-        {/* <i class="bi bi-arrow-right"></i> */}
       </div>
       <div className="other-phones">
-        {otherPhonesData.slice(0, 4).map((c) => {
-          return <OtherPhonesCard {...c} />;
+        {otherPhonesData.slice(0, visible).map((phone, index) => {
+          return <OtherPhonesCard key={index} {...phone} />;
         })}
       </div>
       <div className="button-wrapper">
-        <button>Show more...</button>
+        {showButton && (
+          <button id="btn" onClick={showMore}>
+            Show more...
+          </button>
+        )}
+        {/* The "Show less..." button */}
+        {!showButton && (
+          <button id="btn1" onClick={showLess}>
+            Show less...
+          </button>
+        )}
       </div>
     </div>
   );
 };
+
 const OtherPhonesCard = (props) => {
   const { img, name, price } = props;
   return (
@@ -60,8 +86,8 @@ const OtherPhonesCard = (props) => {
         <p>{name}</p>
         <p>{price}</p>
       </div>
-      
     </div>
   );
 };
+
 export default OtherPhones;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react"
 import ReactDom from "react-dom/client"
 import App from "./app.js"
 
-
 ReactDom.createRoot(document.getElementById('root'))
 .render(<App/>)
